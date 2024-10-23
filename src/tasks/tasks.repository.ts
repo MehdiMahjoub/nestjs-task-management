8d338cdf1dd0f5aa0378f265dcfd06df0c0cc2d8
MehdiMahjoub <mahjoub.mahdi5@gmail.com>
2024-10-23 12:45:18 +0200

    config step

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,13 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from 'src/auth/user.entity';
-
+import { Logger } from '@nestjs/common';
 @Injectable()
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository', { timestamp: true });
   constructor(private dataSource: DataSource) {
     super(Task, dataSource.createEntityManager());
   }
@@ -30,9 +35,13 @@ export class TasksRepository extends Repository<Task> {
         },
       );
     }
-
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(`Failed to get tasks for user "${user.username}"`);
+      throw new InternalServerErrorException();
+    }
   }
 
   async deleteTask(id: string, user: User): Promise<void> {
